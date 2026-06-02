2e9ff293b3a72a53a341cc2f4a3ade472d113b4b
luiscastrillo4289-cmyk <luiscastrillo4289@politecnicomayor.edu.co>
2026-06-02 00:25:49 -0500

    Usar variable de entorno MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const conectarDB = async () => {
   try {
 
-    await mongoose.connect(
-      'mongodb://luiscastrillo4289_db_user:OXE6sopJJZb4mI1w@ac-0rhqj0w-shard-00-00.wa2vseq.mongodb.net:27017,ac-0rhqj0w-shard-00-01.wa2vseq.mongodb.net:27017,ac-0rhqj0w-shard-00-02.wa2vseq.mongodb.net:27017/agencia_viajes?ssl=true&replicaSet=atlas-bycyxw-shard-0&authSource=admin&retryWrites=true&w=majority'
-    )
+    await mongoose.connect(process.env.MONGODB_URI)
 
     console.log('MongoDB conectado correctamente')
 
